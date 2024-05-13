9cd0e4ed476dba0b840fa73b5449dac560564d94
quang1810 <n.trongquang2002@gmail.com>
2024-05-13 23:12:31 +0700

    change be to postgres

--- Frontend/src/sections/products/product-card.jsx
+++ Frontend/src/sections/products/product-card.jsx
@@ -5,36 +5,20 @@ import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 import { fCurrency } from 'src/utils/format-number';
-
-import Label from 'src/components/label';
-import { ColorPreview } from 'src/components/color-utils';
 
 // ----------------------------------------------------------------------
 
 export default function ShopProductCard({ product }) {
-  const renderStatus = (
-    <Label
-      variant="filled"
-      color={(product.status === 'sale' && 'error') || 'info'}
-      sx={{
-        zIndex: 9,
-        top: 16,
-        right: 16,
-        position: 'absolute',
-        textTransform: 'uppercase',
-      }}
-    >
-      {product.status}
-    </Label>
-  );
-
   const renderImg = (
     <Box
       component="img"
       alt={product.name}
-      src={product.cover}
+      src={product.image}
       sx={{
         top: 0,
         width: 1,
@@ -54,9 +38,7 @@ export default function ShopProductCard({ product }) {
           color: 'text.disabled',
           textDecoration: 'line-through',
         }}
-      >
-        {product.priceSale && fCurrency(product.priceSale)}
-      </Typography>
+       />
       &nbsp;
       {fCurrency(product.price)}
     </Typography>
@@ -65,8 +47,6 @@ export default function ShopProductCard({ product }) {
   return (
     <Card>
       <Box sx={{ pt: '100%', position: 'relative' }}>
-        {product.status && renderStatus}
-
         {renderImg}
       </Box>
 
@@ -76,7 +56,9 @@ export default function ShopProductCard({ product }) {
         </Link>
 
         <Stack direction="row" alignItems="center" justifyContent="space-between">
-          <ColorPreview colors={product.colors} />
+          <IconButton color="primary" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
           {renderPrice}
         </Stack>
       </Stack>
